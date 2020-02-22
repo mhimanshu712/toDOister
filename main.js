fff500104fe67f10d44459abd5d861bc1a6abd7f
Himanshu Mishra <mhimanshu712@gmail.com>
2020-02-22 13:43:25 +0530

    Second comm

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow,Menu} = electron;
 
 let mainWindow;
+let addWindow;
 
 //Listen for app to be ready
 app.on('ready',function(){
@@ -17,5 +18,79 @@ app.on('ready',function(){
         slashes: true
     }));
 
+    //Quit Whole app when closed
+    mainWindow.on('closed',()=>{
+        app.quit();
+    })
+
     //the text above basically passes file://dirname/mainWindow.html
+
+    //Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
 })
+
+//Handle create add window
+function creteAddWindow(){
+    //create new window
+    addWindow = new BrowserWindow({
+        width:200,
+        height: 300,
+        title: 'Add Shopping List Item'
+    });
+    //Load html into window
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname,'addWindow.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    //Grabage collection handle
+    addWindow.on('close',function(){
+        addWindow = null;
+    })
+
+}
+
+//Create menu template
+const mainMenuTemplate = [
+    {
+        label:'File',
+        submenu:[
+            {
+                label: 'Add Item',
+                click(){
+                    creteAddWindow();
+                }
+            },
+            {
+                label: 'Clear Items',
+            },
+            {
+                label:'Quit',
+                accelerator: process.platform == 'darwin' ? 'command+Q':'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+]
+
+
+//If not in production add Dev tools
+if(process.env.NODE_ENV !== 'production'){
+    mainMenuTemplate.push({
+        label: 'Dev Tools',
+        submenu:[
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin'?'Command+I':'Ctrl+I',
+                click(item,focusedWindow){
+                    focusedWindow.toggleDevTools();
+                    
+                }
+            }
+        ]
+    })
+}
